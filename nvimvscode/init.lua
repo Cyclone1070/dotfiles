8ed40efca85d11a3c585d9c5097f10d7c69b9c67
RyanMai <hoangmai1070@gmail.com>
2025-03-05 21:55:18 +1100

    added ideavim and neovide like cursor animation

--- nvimvscode/init.lua
+++ nvimvscode/init.lua
@@ -1,16 +1,23 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
---remap leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+
+-- remap for escape visual mode
+map("v", "v", "<Esc>", opts)
 -- enter command remap
-map("n", ";", ":")
+map("n", ";", ":", opts)
 -- remap go to end of line
-map("n", "E", "$")
-map("v", "E", "$")
-map("v", "B", "^")
+map("n", "E", "$", opts)
+map("v", "E", "$", opts)
 -- remap go to start of line
-map("n", "B", "^")
+map("v", "B", "^", opts)
+map("n", "B", "^", opts)
+-- remap pressing space to get hover info
+map("n", " ", vim.lsp.buf.hover, opts)
+-- remap for faster scrolling
+map("n", "J", "5j", opts)
+map("n", "K", "5k", opts)
+map("v", "J", "5j", opts)
+map("v", "K", "5k", opts)
 -- remap tab
 vim.cmd("nmap H gT")
 vim.cmd("nmap L gt")
@@ -18,7 +25,7 @@ vim.cmd("nmap L gt")
 vim.cmd("nmap <C-h> <C-w>h")
 vim.cmd("nmap <C-l> <C-w>l")
 -- move cursor in insert mode
-map("i", "<C-h>", "<Left>")
-map("i", "<C-j>", "<Down>")
-map("i", "<C-k>", "<Up>")
-map("i", "<C-l>", "<Right>")
+map("i", "<C-h>", "<Left>", opts)
+map("i", "<C-j>", "<Down>", opts)
+map("i", "<C-k>", "<Up>", opts)
+map("i", "<C-l>", "<Right>", opts)
