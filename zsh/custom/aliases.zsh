c88da2b730e6f3747c3b72c7d118798cf70a3a1c
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2025-08-06 12:40:51 +1000

    added remotecheck

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -19,6 +19,7 @@ alias geminif="gemini -m gemini-2.5-flash"
 # autossh to enable and disable ssh portforwarding from remote oracle linux instance
 alias remoteon='launchctl load ~/Library/LaunchAgents/com.user.sshtunnel.plist'
 alias remoteoff='launchctl unload ~/Library/LaunchAgents/com.user.sshtunnel.plist'
+alias remotecheck='ps aux | grep autossh'
 
 
 # Setup a tmux session for react workflow
