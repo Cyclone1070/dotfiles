21ea699df3edd17ffa07b83ca78b9c2f86660239
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2025-10-16 14:19:11 +1100

    added brew auto update

--- zsh/custom/exports.zsh
+++ zsh/custom/exports.zsh
@@ -5,3 +5,6 @@ export DOTNET_ROOT="/usr/local/Cellar/dotnet/9.0.7/libexec"
 export PATH="$HOME/Library/Python/3.9/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.dotnet/tools:$PATH"
+
+# Disable Homebrew's built-in auto-update
+export HOMEBREW_NO_AUTO_UPDATE="1"
