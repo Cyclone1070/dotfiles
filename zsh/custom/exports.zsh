5297fee7548b3ad561f170a7fea008a997403d06
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2025-11-24 14:03:34 +1100

    added go bin path

--- zsh/custom/exports.zsh
+++ zsh/custom/exports.zsh
@@ -5,6 +5,7 @@ export DOTNET_ROOT="/usr/local/Cellar/dotnet/9.0.7/libexec"
 export PATH="$HOME/Library/Python/3.9/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.dotnet/tools:$PATH"
+export PATH="/Users/mac/go/bin:$PATH"
 
 # Disable Homebrew's built-in auto-update
 export HOMEBREW_NO_AUTO_UPDATE="1"
