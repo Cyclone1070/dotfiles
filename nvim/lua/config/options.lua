5969cd57ab0fc206c5a36cfa5bde50a00fd41099
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2026-04-02 21:51:24 +1100

    updated indentation

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- Diagnostic styling
 vim.diagnostic.config({
   float = {
     border = "rounded", -- Options: "single", "double", "rounded", "solid", "shadow"
     header = "", -- Optional: removes the "Diagnostics:" header for a cleaner look
   },
 })
+
+-- Indent
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4 -- Number of spaces tabs count for
+opt.softtabstop = 4 -- Number of spaces tabs count for while editing
+opt.expandtab = true -- Use spaces instead of tabs
