290f4efca82bf2b192bd2bed71978999aeeb6214
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2026-09-02 22:26:10 +1000

    turned off copilot and updated lazyvim config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,5 +4,9 @@
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
 --
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Ensure K scrolls up even when an LSP attaches
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.keymap.set({ "n", "v" }, "K", "<C-u>", { buffer = args.buf, desc = "Scroll up" })
+  end,
+})
